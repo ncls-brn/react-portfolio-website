8fa712d3aede0f681b6d4d6e09c3387a6abe48a1
ncls-brn <nicolas.j.baran@gmail.com>
2022-11-28 17:40:14 +0100

    projets css et jsx

--- react-portfolio-website/src/App.jsx
+++ react-portfolio-website/src/App.jsx
@@ -3,9 +3,7 @@ import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
 import Experience from './components/experience/Experience'
-import Services from './components/services/Services'
 import Projets from './components/projets/Projets'
-import Testimonials from './components/testimonials/Testimonials'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 
@@ -17,9 +15,7 @@ const App = () => {
         <Nav />
         <About />
         <Experience />
-        <Services />
         <Projets />
-        <Testimonials />
         <Contact />
         <Footer />
     </>
