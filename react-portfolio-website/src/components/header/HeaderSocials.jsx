cbe0ead7310b833f8654951424ab0b954f8ec8ef
ncls-brn <nicolas.j.baran@gmail.com>
2023-12-06 16:44:41 +0100

    update files

--- react-portfolio-website/src/components/header/HeaderSocials.jsx
+++ react-portfolio-website/src/components/header/HeaderSocials.jsx
@@ -1,16 +1,22 @@
-import React from "react"
-import {BsLinkedin} from "react-icons/bs"
-import {BsGithub} from "react-icons/bs"
-import{BsTwitter} from "react-icons/bs"
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
+import { BsTwitterX } from "react-icons/bs";
 
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href="https://www.linkedin.com/in/nicolas-baran/" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com/ncls-brn" target="_blank"><BsGithub/></a>
-        <a href="https://twitter.com/Nclsjbrn" target="_blank"><BsTwitter/></a>
+      <a href="https://www.linkedin.com/in/nicolas-baran/" target="_blank">
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com/ncls-brn" target="_blank">
+        <BsGithub />
+      </a>
+      <a href="https://twitter.com/Nclsjbrn" target="_blank">
+        <BsTwitterX />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderSocials
+export default HeaderSocials;
