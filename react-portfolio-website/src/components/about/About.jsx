cb485877c27eec7dfd141236c1d705b2406333ff
ncls-brn <nicolas.j.baran@gmail.com>
2023-12-08 11:11:51 +0100

    update about files

--- react-portfolio-website/src/components/about/About.jsx
+++ react-portfolio-website/src/components/about/About.jsx
@@ -1,10 +1,9 @@
-import React from "react"
-import "./about.css"
-import ME from "../../assets/me3.png"
-import {FiAward} from "react-icons/fi"
-import {CgUser} from "react-icons/cg"
-import {BsFiles} from "react-icons/bs"
-
+import React from "react";
+import "./about.css";
+import ME from "../../assets/me3.png";
+import { FiAward } from "react-icons/fi";
+import { CgUser } from "react-icons/cg";
+import { BsFiles } from "react-icons/bs";
 
 const About = () => {
   return (
@@ -22,38 +21,50 @@ const About = () => {
         <div className="about__content">
           <div className="about__cards">
             <article className="about__card">
-              <FiAward className="about__icon"/>
+              <FiAward className="about__icon" />
               <h5>Experience</h5>
-              <small>Student at Ada Tech School<br></br></small>
-              <small> Withings since 2023</small>
+              <small>
+                - Student at Ada Tech School 2022 - today<br></br>
+              </small>
+              <small>- Apprentice at Withings since 2023 - today</small>
             </article>
 
             <article className="about__card">
-              <CgUser className="about__icon"/>
+              <CgUser className="about__icon" />
               <h5>my daily life</h5>
-              <small>I was able to experience through practice many tools and daily gestures of developer  but also several modes of operation.</small>
+              <small>
+                I was able to experience through practice many tools and daily
+                gestures of developer but also several modes of operation.
+              </small>
             </article>
 
             <article className="about__card">
-              <BsFiles className="about__icon"/>
+              <BsFiles className="about__icon" />
               <h5>Projects</h5>
-              <small>I take part in projects in different domains, teams and languages, giving me great adaptability.</small>
+              <small>
+                I take part in projects in different domains, teams and
+                languages, giving me great adaptability.
+              </small>
             </article>
           </div>
 
           <p>
-          After working 10 years as an  Staff Sergeant in the  French Army, I am now doing a reconversion in web development, an area where I discovered the richness of the trades (creativity, teamwork, problem solving, etc.) and the major impact on both companies and society at large. 
-Autonomous, curious, attentive and team-oriented, I always in search to develop and deepen my knowledge while putting my skills at the service of the collective for the success of your projects.
+            After working 10 years as an Staff Sergeant in the French Army, I am
+            now doing a reconversion in web development, an area where I
+            discovered the richness of the trades (creativity, teamwork, problem
+            solving, etc.) and the major impact on both companies and society at
+            large. Autonomous, curious, attentive and team-oriented, I always in
+            search to develop and deepen my knowledge while putting my skills at
+            the service of the collective for the success of your projects.
           </p>
 
-          <a href="#contact" className="btn btn-primary">Let's Talk</a>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
         </div>
-
-
-        </div>
+      </div>
     </section>
+  );
+};
 
-  )
-}
-
-export default About
+export default About;
