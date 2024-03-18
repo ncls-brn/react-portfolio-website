131208e131f20c4cd519da9f6170f95965630df9
ncls-brn <nicolas.j.baran@gmail.com>
2024-03-18 21:39:48 +0100

    update project

--- react-portfolio-website/src/components/projets/Projets.jsx
+++ react-portfolio-website/src/components/projets/Projets.jsx
@@ -138,6 +138,24 @@ const Projets = () => {
           <div className="projets__item-image"></div>
           <img src={IMG7} alt="" className="size-image" />
           <h3>CineMatch(in progress)</h3>
+          <div className="projets__item-cta">
+            <a
+              href="https://github.com/ncls-brn/movie-app-v4"
+              className="btn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+            <a
+              href="https://cinematch-app.vercel.app"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
+              live projet
+            </a>
+          </div>
         </article>
       </div>
     </section>
